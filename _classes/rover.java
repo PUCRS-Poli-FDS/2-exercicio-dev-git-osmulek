e8a1c1b0ab0e078763476f11f7be20e47e3ef3a1
12107429 <12107429@facin.portoalegre.pucrsnet.br>
2019-09-10 20:40:43 -0300

    Merge dev-felipe into master

---
+++ _classes/rover.java
@@ -0,0 +1,20 @@
+// import java.util.Scanner;
+
+// public class Rover {
+//     private String direction, posX, posY;
+
+//     public Rover(String direction, String posX, String posY) {
+//         System.out.println("CONSTRUCTOR DEFINED");
+//         this.direction = direction;
+//         this.posX = posX;
+//         this.posY = posY;
+//     }
+
+//     private String accessDirections() {
+//         return this.direction;
+//     }
+
+//     private String accessPositions(){
+        
+//     }
+// }
