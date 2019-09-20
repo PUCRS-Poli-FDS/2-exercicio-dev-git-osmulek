d5bbc4d73d7c9e9f9862ed5156c35e56d7fcd719
p-karol <priscilla.karolczak@gmail.com>
2019-09-20 19:37:56 +0100

    corrigindo teste unitario

---
+++ _classes/RoverTest.java
@@ -0,0 +1,30 @@
+//import rover.java;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.matchers.JUnitMatchers;
+
+public class RoverTest {
+
+    rover rover = new rover();
+    
+    @Before
+    public void beforeTest() {
+        rover = new rover(location, maxLocation);
+    }
+        
+    @Test
+    public void testCoordinate() {
+        
+        rover.setPosition(1, 2, N);
+        rover.process("LMLMLMLMM");
+        assertThat(containsString(rover.printPosition(), "1 3 N"));
+        
+        rover.setPosition(3, 3, E);
+        rover.process("MMRMMRMRRM");
+        assertThat(containsString(rover.printPosition(), "5 1 E"));
+    }  
+        
+}
